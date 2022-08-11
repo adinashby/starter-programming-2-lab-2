e70999fa97bb638d0167d4d1d3ee184dfb2cb95f
Adin Ashby <47254289+adinashby@users.noreply.github.com>
2022-08-10 21:04:30 -0400

    First Commit.

---
+++ src/main/java/com/prog2/labs/LabTwo.java
@@ -0,0 +1,74 @@
+package com.prog2.labs;
+/**
+ * @author adinashby
+ *
+ */
+public class LabTwo {
+
+	/**
+	 * Write your test code below in the main (optional).
+	 *
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	/**
+	 * 1) Write an if-else statement to compute the amount of shipping due on an online sale. 
+	 * If the cost of the purchase is less than or equal to $20, the shipping cost is $5.99. 
+	 * If the cost of the purchase is over $20 and at most $65, the shipping cost is $10.99. 
+	 * If the cost of the purchase is over $65, the shipping cost is  $15.99. 
+	 * 
+	 * @param costOfPurchase cost of purchase amount.
+	 *
+	 * @return result cost of shipping.
+	 */
+	
+	public double calcAmountOfShipping(double costOfPurchase) {
+		
+		
+		return 0.0;
+	}
+	
+	/**
+	 * 2) Write a Java program that accepts a positive integer number from the user 
+	 * and displays prime numbers until that number. 
+	 * (Reminder: Prime numbers are numbers that are only divisible by 1 and  themselves) 
+	 * 
+	 * @param input a positive number.
+	 *
+	 * @return result a string containing all the prime numbers up to the input.
+	 */
+	
+	public String findPrimeNumbersUptoInput(int input) {
+		
+		
+		return "";
+	}
+	
+	/**
+	 * 3) Write a Java program that returns the following shape based on the number of rows:
+	 * 
+	 * 1
+	 * 2 3
+	 * 4 5 6
+	 * 7 8 9 10
+	 * 11 12 13 14 15
+	 * 
+	 * @param numberOfRows number of rows.
+	 *
+	 * @return result a string containing the shape.
+	 */
+	
+	public String generateShape(int numberOfRows) {
+		
+		
+		return "";
+	}
+	
+	/**
+	 * 4) Complete the Account class (see inside the class for instruction). 
+	 * 
+	 */
+
+}
